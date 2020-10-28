ec86910baec67367adfd6523762ad4ece49434bf
Otavio Henrique <OtavioHenLimSil@gmail.com>
2020-10-28 20:18:50 -0300

    Commit 6 dos arquivos

--- templates/admin.tpl.php
+++ templates/admin.tpl.php
@@ -7,8 +7,35 @@
         <link rel="stylesheet" href="/css/bootstrap.min.css">
     </head>
     <body>
-        <h1>Administração</h1>
-        <?php include $content ?>
+        <!-- <h1>Administração</h1> -->
+        <div id="header">
+            <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+                <a href="" class="navbar-brand">AdminSON</a>
+                <span class="navbar-text">
+                    Painel administrativo
+                </span>
+            </nav>
+        </div>
+        <div id="main">
+            <div class="row">
+                <div class="col">
+                    <ul class="nav flex-column nav-pills bg-secondary text-white p-2">
+                        <li class="nav-item">
+                            <span class="nav-link text-white-50"><small>Menu</small></span>
+                        </li>
+                        <li class="nav-item">
+                            <a href="" class="nav-link active">Páginas</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="" class="nav-link">Usuários</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="col-10">
+                    <?php include $content ?>
+                </div>
+            </div>
+        </div>
         <script src="/js/jquery-3.5.1.min.js"></script>
         <script src="https://unpkg.com/@popperjs/core@2"></script>
         <script src="/js/bootstrap.min.js"></script>
